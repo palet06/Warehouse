1d736e614e8f4f4b199751d1a397601746ca54d0
Murat Hayaloglu <murath31@hotmail.com>
2024-12-23 13:36:22 +0300

    bazı şeyler

---
+++ app/(root)/dashboard/layout.tsx
@@ -0,0 +1,17 @@
+
+
+export default function DashboardLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en">
+      <body
+        
+      >
+        {children}
+      </body>
+    </html>
+  );
+}
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,18 +1,48 @@
 import type { Config } from "tailwindcss";
 
 export default {
-  content: [
+    darkMode: ["class"],
+    content: [
     "./pages/**/*.{js,ts,jsx,tsx,mdx}",
     "./components/**/*.{js,ts,jsx,tsx,mdx}",
     "./app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
-    extend: {
-      colors: {
-        background: "var(--background)",
-        foreground: "var(--foreground)",
-      },
-    },
+  	extend: {
+  		colors: {
+  			background: 'var(--background)',
+  			foreground: 'var(--foreground)',
+  			csgbBgRed: '#DC0D15',
+  			csgbMenuBgRed: '#C71616',
+  			csgbBaslikBlack: '#151515',
+  			csgbParagraphBlack: '#333333',
+  			sidebar: {
+  				DEFAULT: 'hsl(var(--sidebar-background))',
+  				foreground: 'hsl(var(--sidebar-foreground))',
+  				primary: 'hsl(var(--sidebar-primary))',
+  				'primary-foreground': 'hsl(var(--sidebar-primary-foreground))',
+  				accent: 'hsl(var(--sidebar-accent))',
+  				'accent-foreground': 'hsl(var(--sidebar-accent-foreground))',
+  				border: 'hsl(var(--sidebar-border))',
+  				ring: 'hsl(var(--sidebar-ring))'
+  			}
+  		},
+  		fontFamily: {
+  			rubik: [
+  				'Rubik',
+  				'sans-serif'
+  			],
+  			nunito: [
+  				'Nunito',
+  				'serif'
+  			]
+  		},
+  		borderRadius: {
+  			lg: 'var(--radius)',
+  			md: 'calc(var(--radius) - 2px)',
+  			sm: 'calc(var(--radius) - 4px)'
+  		}
+  	}
   },
-  plugins: [],
+  plugins: [import("tailwindcss-animate")],
 } satisfies Config;
